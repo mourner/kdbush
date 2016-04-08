9077b60f047ce39f4e0fec0902b210ffa8638945
Vladimir Agafonkin <agafonkin@gmail.com>
2016-04-07 19:30:25 -0700

    use Int32Array in the bench

--- bench.js
+++ bench.js
@@ -10,7 +10,7 @@ for (var i = 0; i < 1000000; i++) {
 console.time('index ' + points.length + ' points');
 var index = kdbush(points,
     (p) => p.x,
-    (p) => p.y);
+    (p) => p.y, 64, Int32Array);
 console.timeEnd('index ' + points.length + ' points');
 
 console.time('10000 small bbox queries');
