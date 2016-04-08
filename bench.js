006b5823650743a0fe643784ce6c014401cc6513
Vladimir Agafonkin <agafonkin@gmail.com>
2016-04-08 16:19:02 -0700

    add memory report to bench

--- bench.js
+++ bench.js
@@ -1,15 +1,21 @@
 'use strict';
 
 var kdbush = require('./src/kdbush');
+var v8 = require('v8');
 
 var randomPoint = (max) => { return {x: randomInt(max), y: randomInt(max)}; };
 var randomInt = (max) => Math.floor(Math.random() * max);
+var heapSize = () => v8.getHeapStatistics().used_heap_size / 1000 + ' KB';
 
 for (var i = 0, points = []; i < 1000000; i++) points.push(randomPoint(1000));
+
+console.log('memory: ' + heapSize());
 
 console.time('index ' + points.length + ' points');
 var index = kdbush(points, (p) => p.x, (p) => p.y, 64, Int32Array);
 console.timeEnd('index ' + points.length + ' points');
+
+console.log('memory: ' + heapSize());
 
 console.time('10000 small bbox queries');
 for (i = 0; i < 10000; i++) {
