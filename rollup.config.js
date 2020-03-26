c77ed785add47f6722af7cc1d71cada233325ae2
Vladimir Agafonkin <agafonkin@gmail.com>
2020-03-27 00:15:41 +0200

    fix build config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import {terser} from 'rollup-plugin-terser';
 import buble from '@rollup/plugin-buble';
 
 const config = (file, plugins) => ({
-    input: 'src/index.js',
+    input: 'index.js',
     output: {
         name: 'KDBush',
         format: 'umd',
